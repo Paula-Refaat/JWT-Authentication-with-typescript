60492b21ee404402d37cede235f026f6f0363f22
Paula <poularefaat86@gmail.com>
2024-08-29 23:46:08 +0300

    🧪 Adding Jasmine tests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-console.log('Hello world!');
+import express, { Application, Request, Response } from 'express';
 
-let number;
+const PORT = 3000;
 
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello World' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;

887aa1332baf7edec1cec1ea316a0d4542888127
Paula <poularefaat86@gmail.com>
2024-08-30 18:26:04 +0300

    chore: Add middleware for logging, security

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import RateLimit, { rateLimit } from 'express-rate-limit';
 
 const PORT = 3000;
 
 const app: Application = express();
+
+app.use(express.json());
+
+app.use(morgan('common'));
+
+app.use(helmet());
+
+app.use(
+  rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: 'Too many requests, please try again later',
+  }),
+);
+
+app.post('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello World', data: req.body });
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello World' });
